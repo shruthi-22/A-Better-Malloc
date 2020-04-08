8b174b2f84a8ad78d052fa9963202ba1c4eaf0f1
shruthi-22 <shruthi.palanisamy.20@gmail.com>
2020-04-09 01:05:58 +0530

    modify IsValid

--- main_1.c
+++ main_1.c
@@ -4,53 +4,36 @@
 #include <unistd.h>
 #include "linkedList.h"
 
-int no_pages(int bytes)    //when bytes required by a process is passed, no_pages() returns the no. of pages to be allocated for that process.
-{
-	 if(bytes % getpagesize()==0)
-        	return bytes/getpagesize();
-    	else
-        	return bytes/getpagesize()+1;
-
-}
-
-/*int bytes_allocated(int bytes)   // returns the total number of bytes allocated
-{
-	int page_size,size_alloc;
-
-	page_size = getpagesize();
-
-        if((bytes%page_size)!=0)
-        {
-                size_alloc = bytes - (bytes%page_size) + page_size;
-        }
-        else
-        {
-                size_alloc = bytes;
-        }
-	return size_alloc;
-}*/
-
 void *ptr=NULL;
 int allocated = 0;
 int allotted = 0;
 
+int no_pages(int bytes)    //when bytes required by a process is passed, no_pages() returns the no. of pages to be allocated for that process.
+{
+	 if(bytes % getpagesize()==0)
+        return bytes/getpagesize();
+    else
+        return bytes/getpagesize()+1;
+
+}
+
 int Mem_Init(int sizeOfRegion)			// function to initialise memory to the process
 {
-        if(sizeOfRegion < 0)
-        {
-                printf("size of the Region passed to Mem_Init should be positive");
-                return 0;
-        }
-        int n=no_pages(sizeOfRegion);
+    if(sizeOfRegion < 0)
+    {
+        printf("Size of the Region passed to Mem_Init should be positive");
+        return 0;
+    }
+    int n=no_pages(sizeOfRegion);
 	int PAGESIZE = getpagesize();
-        ptr = mmap(NULL,n*PAGESIZE,PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-        allotted=sizeOfRegion;
-        if(ptr == MAP_FAILED)
-        {
-                printf("Error in mmap");
-                return 0;
-        }
-        return 1;
+    ptr = mmap(NULL,n*PAGESIZE,PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    allotted=sizeOfRegion;
+    if(ptr == MAP_FAILED)
+    {
+        printf("Error in mmap");
+        return 0;
+    }
+    return 1;
 }
 
 void *Mem_Alloc(int size)		//allocates the desirered amout of memory from the total available
@@ -59,19 +42,19 @@ void *Mem_Alloc(int size)		//allocates the desirered amout of memory from the to
 	if(size > 0)
 	{
 		if(head==NULL)          //if head is NULL,simply insert
-                {
-                        insert(ptr,size);
+        {
+			insert(ptr,size);
 			allocated += size;
-                        return head->start;
+            return head->start;
 		}
 		else
 		{
-			if(allocated+size <= allotted)			
+			if(allocated+size <= allotted)		//Space might be available
 			{
 				struct Node* temp=head,*prevNode;
 				while(temp!=NULL)				// find the best-fit size for the memory asked
 				{
-					if(temp->status == 0 )
+					if(temp->status == 0 )		//if that area is free
 					{
 						if(temp->size >= size && temp->size <= req)
 						{
@@ -84,12 +67,11 @@ void *Mem_Alloc(int size)		//allocates the desirered amout of memory from the to
 					temp=temp->link;
 				}
 				if(temp==NULL && flag == 0)           //insert node at last
-                                {
-					
-                                        insert(prevNode->start + prevNode->size, size);
+                {
+					insert(prevNode->start + prevNode->size, size);
 					allocated +=size;
 					return (prevNode->start + prevNode->size);
-                                }
+                }
 				else							// if a free memory satisfies the best fit costraint
 				{
 					struct Node* temp = head;
@@ -124,7 +106,7 @@ int Mem_Free(void *ptr)
 	struct Node* temp = head;
 	while(temp!=NULL)
 	{
-		if( temp->start <= ptr && temp->link->start > ptr)			//locates the pointer in the list
+		if( temp->start <= ptr && temp->start + temp->size > ptr)			//locates the pointer in the list
 		{
 			if(temp->status != 0)						//checks if the given pointer is allocted
 			{
@@ -134,7 +116,7 @@ int Mem_Free(void *ptr)
 				//printf("%d  %u  %u",size_free,ptr,temp->start);
 				if(size_free != 0)					//checks if the given pointer points to base addr or intermedite address
 				{
-					struct Node* N = New();
+					struct Node* N = New(0);
 					temp->size = temp->size - size_free;
 					N->size = size_free;
 					N->link = temp->link;
@@ -143,7 +125,7 @@ int Mem_Free(void *ptr)
 				}
 				else
 				{
-					temp->start = NULL;
+					temp->start = NULL;	//
 					temp->status = 0;
 					allocated -= size_free;
 				}
@@ -162,8 +144,19 @@ int Mem_Free(void *ptr)
 
 int Mem_IsValid(void *p)
 {
-	void *last = ptr+allotted;
-	if( p >= ptr && p <= last)
+	struct Node* temp=head;
+	while(temp != NULL)
+	{
+		if ( p >= temp->start && p < temp->start + temp->size )
+			break;
+		temp = temp->link;
+	}
+	if(temp==NULL)
+	{
+		printf("\n\nInvalid pointer passed");
+		return -1;
+	}
+	if(temp->status==1)
 	{
 		return 1;
 	}
@@ -183,9 +176,7 @@ int Mem_GetSize(void *p)
 		}
 	}
 	else
-	{
 		return -1;
-	}
 }
 
 int main()
@@ -206,27 +197,37 @@ int main()
         void *d = Mem_Alloc(50);
         void *e = Mem_Alloc(70);
         void *f = Mem_Alloc(200);
+
         printf("\n Initial linked list : ");
         traverse(head);
+
 	int ret = Mem_Free(b+18);
+	printf("\n Freeing 20 linked list : ");
+	traverse(head);
 	if (ret == 0 )
 		printf("\n Memory Freed successfully ");
 	else if(ret == 1)
 		printf("\n No operation occurred ");
-	else 
+	else
 		printf("\n Memory is already free ");
-	printf("\n Freeing 20 linked list : ");
-        traverse(head);
+
+
 	b = Mem_Alloc(18);
-	printf("\n Allotting 20 linked list : ");
+	printf("\n Allotting 18 linked list : ");
         traverse(head);
+
 	printf("\n\t Total allocated memory :  %d and total allotted : %d unused : %d ",allocated,allotted,allotted - allocated);
-	int valid = Mem_IsValid(f);
+
+	int valid = Mem_IsValid(f+200);
 	if( valid == 1 )
 		printf("\n\t Memory is valid ");
 	else
 		printf("\n\t Memory is invalid ");
-	int size = Mem_GetSize(d);
+
+	int k=Mem_Free(f+198);
+	printf("Freed");
+	void *g;
+	int size = Mem_GetSize(g);
 	if(size == -1)
 		printf("\n Not a valid memory ");
 	else
