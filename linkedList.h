4c93fe2fd8a2597aac04d8b635b7845c0abe01a3
Ananya <ananyasandilya1@gmail.com>
2020-04-19 16:41:21 +0530

    add insert_sorted

--- linkedList.h
+++ linkedList.h
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 struct Node
 {
     void* start;	//to point to memory allocated
@@ -6,13 +8,14 @@ struct Node
     struct Node* link;
 };
 
-struct Node *head = NULL;
+struct Node *head = NULL,*head1 = NULL;
 
-struct Node* New(int sz)
+struct Node* New()
 {
     struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
     temp->start=NULL;
-    temp->size=sz;
+//    temp->size=sz;
+    temp->size=0;
     temp->status=0;
     temp->link=NULL;
     return temp;
@@ -20,11 +23,12 @@ struct Node* New(int sz)
 
 struct Node* insert(void *p,int e)
 {
-   	struct Node* newNode= New(e);
+   	struct Node* newNode= New();
 	if(head==NULL)  //empty ll
     	{
 		newNode -> start = p;	//here, p i the address returned by mmap
 		newNode -> status = 1;
+		newNode -> size = e;
 		head=newNode;
 		return head;
 	}
@@ -36,6 +40,7 @@ struct Node* insert(void *p,int e)
 
 	newNode -> start = p;
 	newNode -> status = 1;
+	newNode ->size = e;
 	temp -> link = newNode ;
 	return head;
 }
@@ -51,15 +56,69 @@ void traverse(struct Node *head)
     }
 }
 
-/*
-int main()
+struct Node* insert_sorted(int sizeRequired)
 {
-	insert(2);
-	insert(4);
-	insert(7);
-	insert(8);
-	insert(9);
-	traverse(head);
+   struct Node *N = New();
+   N -> size = sizeRequired; 
+   if(head1==NULL)  //empty ll
+   {
+        head1=N;
+        return head1;
+
+   }
+   else
+   {
+	struct Node *temp = head1,*prevNode=NULL;
+	while(temp->link != NULL)
+	{
+		if(temp-> size <= sizeRequired && temp->link->size > sizeRequired)
+		{
+			N->link = temp->link;
+			temp->link = N;
+			return head1;
+		}
+		else if(temp->size > sizeRequired)
+		{
+			N->link = temp->link;
+			head1 = N;
+			return head1;
+		}
+		prevNode = temp;
+		temp = temp->link;
+	}
+	if(temp->link ==NULL)
+	{
+		if (sizeRequired > temp->size)
+		{
+			temp->link = N; 
+			return head1;
+		}
+		else
+		{
+			if(prevNode == NULL)
+			{
+				N->link = temp;
+				head1 = N;
+				return head;
+			}
+			else
+			{
+				N->link = temp;
+				prevNode->link = N;
+				return head1;
+			}
+		}
+	}
+   }
 }
 
-*/
+
+int main()
+{
+	insert_sorted(10);
+	insert_sorted(1);
+	insert_sorted(7);
+	insert_sorted(3);
+	insert_sorted(9); 
+	traverse(head1);
+}
